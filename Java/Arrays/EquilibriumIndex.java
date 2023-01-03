a9070ea59b23628e74ed8f5663ca2de55272b29d
Prabhu Palani <55530600+prabhupalani18@users.noreply.github.com>
2023-01-03 14:38:29 +0530

    Prefix sum and Equilibrium index count

---
+++ Java/Arrays/EquilibriumIndex.java
@@ -0,0 +1,37 @@
+package Arrays;
+
+//Given N array elements, Count the no. of equilibrium index. i.e: sum of all elements on left of ith index == sum of all elements on right of ith index
+//Time complexity - O(n), Space complexity - O(1)
+
+public class EquilibriumIndex {
+    public static int[] prefixSum(int[] arr)
+    {
+        int n = arr.length;
+        int[] pSum = new int[n];
+        pSum[0] = arr[0];
+        for(int i=1;i<n;i++)
+        {
+            pSum[i] = pSum[i-1] + arr[i];
+        }
+        return pSum;
+    }
+
+    public static int countEqIndex(int[] arr)
+    {
+        int[] pSum = prefixSum(arr);
+        int count = 0;
+        int n = arr.length;
+        for(int i=0;i<n;i++)
+        {
+            if(pSum[i] == pSum[n-1]-pSum[i])
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int[] arr = {-7,1,5,2,-4,3,0};
+        System.out.println(countEqIndex(arr));
+    }
+}
