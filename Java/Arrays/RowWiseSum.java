67ebc043e80974add06cb207b5af54d8f583c897
Prabhu Palani <55530600+prabhupalani18@users.noreply.github.com>
2023-01-21 15:59:14 +0530

    Row wise sum in 2d array

---
+++ Java/Arrays/RowWiseSum.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+//Given an 2 array of N*M elements, find sum of each row.
+//Time complexity - O(n^2), Space complexity - O(1)
+
+public class RowWiseSum {
+
+    public static void findRowWiseSum(int[][] arr){
+        int n = arr.length;
+        int m = arr[0].length;
+        for(int i=0;i<n;i++)
+        {
+            int sum=0;
+            for(int j=0;j<m;j++)
+            {
+                sum=sum+arr[i][j];
+            }
+            System.out.println(sum);
+        }
+    }
+    public static void main(String[] args) {
+        int[][] arr = {
+            {1,2,3,4,5},
+            {6,7,8,9,5},
+            {14,5,7,2,-1}
+        };
+        findRowWiseSum(arr);
+    }
+}
