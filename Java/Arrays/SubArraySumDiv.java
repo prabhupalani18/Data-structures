51d4d5159267b97e85d443f94b6e937e67d4a70f
Prabhu Palani <55530600+prabhupalani18@users.noreply.github.com>
2023-02-14 22:34:08 +0530

    Subarray sum divisible by k using HashMap

--- Java/Arrays/SubArraySumDiv.java
+++ Java/Arrays/SubArraySumDiv.java
@@ -1,8 +1,11 @@
 package Arrays;
 
+import java.util.HashMap;
+
 //* Given an array of length n and an integer k, find the count of all non-empty subarrays of the array that have a sum divisible by k.
 //* Time complexity for findDivCount1 - O(n^3), Space complexity - O(1) ~ 10^15 TLE
 //* Time complexity for findDivCount2 - O(n^2), Space complexity - O(1) ~ 10^10 TLE
+//* Time complexity for findDivCount3 - O(n), Space complexity - O(n)
 
 public class SubArraySumDiv {
 
@@ -47,11 +50,51 @@ public class SubArraySumDiv {
         return count;
     }
 
+    public static int[] prefixSum(int[] arr)
+    {
+        int n=arr.length;
+        int[] pSum = new int[n];
+        pSum[0] = arr[0];
+        for(int i=1;i<n;i++)
+        {
+            pSum[i]=arr[i]+pSum[i-1];
+        }
+        return pSum;
+    }
+
+    public static int findDivCount3(int[] arr,int k)
+    {
+        int n=arr.length;
+        int[] pSum=prefixSum(arr);
+        int count=0;
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        hm.put(0, 1);
+        int[] remArr = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            remArr[i]=arr[i]%k;
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(hm.containsKey(remArr[i]))
+            {
+                count+=hm.get(remArr[i]);
+                hm.put(remArr[i],hm.get(remArr[i])+1);
+            }
+            else{
+                hm.put(remArr[i],1);
+            }
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
         int[] arr = {2,7,6,1,4,5};
         int res1 = findDivCount1(arr, 3);
         System.out.println(res1);
         int res2 = findDivCount2(arr, 3);
         System.out.println(res2);
+        int res3 = findDivCount3(arr, 3);
+        System.out.println(res3);
     }
 }
